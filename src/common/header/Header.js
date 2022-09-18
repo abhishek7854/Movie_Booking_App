9fc558d2677e7dfb5b296db59e9d50a0d24a1ba5
abhishek7854 <bagdeabhishek9@gmail.com>
2022-09-18 15:35:03 +0530

    Checkpoint 2

---
+++ src/common/header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <img src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg" alt="" className="header-logo" />
+      </div>
+    )
+  }
+}
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,1 +1,13 @@
-import 'Home.css';
+import React, { Component } from 'react'
+import './Home.css';
+import Header from '../../common/header/Header';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+}
